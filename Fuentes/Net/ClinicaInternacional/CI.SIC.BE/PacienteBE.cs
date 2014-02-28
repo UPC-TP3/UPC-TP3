7c02ace70ce8565ca2a7974fbd9175609e278a93
Julio Salvatierra <jsalvatierrab@gmail.com>
2014-02-28 15:10:07 -0500

    JSB

--- Fuentes/Net/ClinicaInternacional/CI.SIC.BE/PacienteBE.cs
+++ Fuentes/Net/ClinicaInternacional/CI.SIC.BE/PacienteBE.cs
@@ -8,8 +8,10 @@ namespace CI.SIC.BE
     [Serializable]
     public class PacienteBE
     {
+        public int Id_paciente { get; set; }
         public string DNI { get; set; }
         public string Nombres { get; set; }
+        public int Apellidos { get; set; }
         public string Estado { get; set; }
 
    }
