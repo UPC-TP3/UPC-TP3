0d01145a796c143b1aee8b0511458204b2ce11d5
Julio Salvatierra <jsalvatierrab@gmail.com>
2014-02-28 14:08:38 -0500

    JSB

---
+++ Fuentes/Net/ClinicaInternacional/CI.SIC.BE/MedicoBE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CI.SIC.BE
+{
+    [Serializable]
+    public class MedicoBE
+    {
+        public int Id_medido { get; set; }
+        public string Nom_medico { get; set; }
+        public string Ape_medico { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+    }
+}
