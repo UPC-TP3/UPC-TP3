45184e981c7892291d7dbc6a02c989a5d4f38519
Julio Salvatierra <jsalvatierrab@gmail.com>
2014-03-04 21:57:07 -0500

    JSB

--- Fuentes/Net/ClinicaInternacional/CI.SIC.BE/Programacion_ExamenBE.cs
+++ Fuentes/Net/ClinicaInternacional/CI.SIC.BE/Programacion_ExamenBE.cs
@@ -9,12 +9,24 @@ namespace CI.SIC.BE
     public class Programacion_ExamenBE
     {
         public int Id_programacion { get; set; }
-        public int Id_consultorio { get; set; }
-        public int Id_examen { get; set; }
-        public int Id_personal { get; set; }
-        public DateTime Fecha { get; set; }
-        public TimeZone Hora { get; set; }
-        public string Estado { get; set; }
         public int Id_orden_examen { get; set; }
+        public int Id_horario { get; set; }
+        public string Estado { get; set; }
+
+        public int Id_consultorio { get; set; }
+        public int Id_local { get; set; }
+        public int Id_examen { get; set; }
+        public int Id_especialista { get; set; }
+        public string Especialista { get; set; }
+        public DateTime Fecha { get; set; }
+        //public string Hora { get; set; }
+        
+        
+        public int Id_historia { get; set; }
+        public string Local { get; set; }
+        public string Consultorio { get; set; }
+        public string Examen { get; set; }
+        public string Paciente { get; set; }
+
     }
 }
