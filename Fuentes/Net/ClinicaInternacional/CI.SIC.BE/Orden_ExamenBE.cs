a4d787777a1b6b3ac57ae52f63bcaba5d6c56639
Julio Salvatierra <jsalvatierrab@gmail.com>
2014-03-04 21:45:30 -0500

    jsb

--- Fuentes/Net/ClinicaInternacional/CI.SIC.BE/Orden_ExamenBE.cs
+++ Fuentes/Net/ClinicaInternacional/CI.SIC.BE/Orden_ExamenBE.cs
@@ -10,8 +10,16 @@ namespace CI.SIC.BE
     {
         public int Id_orden_examen { get; set; }
         public int Id_medico { get; set; }
+        public string Medico { get; set; }
         public int Id_orden_atencion { get; set; }
+        public string Tipo_atencion { get; set; }
+        public string Paciente { get; set; }
+        public int Id_historia { get; set; }
         public DateTime Fecha { get; set; }
+        public int Id_examen { get; set; }
+        public int Id_tpo_examen { get; set; }
+        public string Examen { get; set; }
         public string Estado { get; set; }
+        public decimal Precio { get; set; }
     }
 }
