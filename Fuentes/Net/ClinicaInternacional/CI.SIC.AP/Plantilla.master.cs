ca82f9615dd1b4593ea571c3f756020b69bf97da
lbenites87 <lbenites87@gmail.com>
2014-03-05 22:57:03 -0500

    Creacion de menu y master

--- Fuentes/Net/ClinicaInternacional/CI.SIC.AP/Plantilla.master.cs
+++ Fuentes/Net/ClinicaInternacional/CI.SIC.AP/Plantilla.master.cs
@@ -9,6 +9,21 @@ public partial class Plantilla : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        Menu_load();
+    }
+    private void Menu_load()
+    {
+        string menu = string.Empty;
+        menu += string.Format("<li><a href=\"#\">{0}</a><ul class=\"sub\">", "Admisión");
+        menu += string.Format("<li><a href=\'../{0}\'>{1}</a></li>", "GestionAdmision/RegistrarGestionAmbulatoria.aspx", "Ambulatoria");
+        menu += string.Format("<li><a href=\'../{0}\'>{1}</a></li>", "GestionAdmision/GcAdmEmergencia.aspx", "Emergencia");
+        menu += string.Format("<li><a href=\'../{0}\'>{1}</a></li>", "GestionAdmision/GcAdmHospitalizacion.aspx", "Hospitalizacion");
+        menu += "</ul></li>";
 
+        menu += string.Format("<li><a href=\"#\">{0}</a><ul class=\"sub\">", "Citas");
+        menu += string.Format("<li><a href=\'../{0}\'>{1}</a></li>", "GestionAdmision/RegistrarGestionAmbulatoria.aspx", "Citas");
+        menu += "</ul></li>";
+
+        lstMenu.Text = menu;
     }
 }
