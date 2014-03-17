22518e46714d14f82023db3d17ad4647c275f78e
juan25vito <juan25vito@gmail.com>
2014-03-17 15:31:00 -0500

    Gestión de Mantenimiento 75%

---
+++ Fuentes/Net/ClinicaInternacional/CI.SIC.BL/BL_SolicitudMantenimiento.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CI.SIC.BE;
+using CI.SIC.DA;
+
+namespace CI.SIC.BL
+{
+    public class BL_SolicitudMantenimiento : BaseBL<BL_SolicitudMantenimiento>
+    {
+        public List<BE_SolicitudMantenimiento> fn_ListaSM(string fi, string ff)
+        {
+            List<BE_SolicitudMantenimiento> result = null;
+            result = DAO_SolicitudMantenimiento.Instancia.fn_ListaSM(fi, ff);
+            return result;
+        }
+
+        public bool fn_InsertarSM(BE_SolicitudMantenimiento p_SMBE)
+        {
+            bool result = false;
+            result = DAO_SolicitudMantenimiento.Instancia.fn_InsertarSM(p_SMBE);
+
+            return result;
+        }
+
+        public bool fn_ActualizarSM(BE_SolicitudMantenimiento p_SMBE)
+        {
+            bool result = false;
+            result = DAO_SolicitudMantenimiento.Instancia.fn_ActualizarSM(p_SMBE);
+
+            return result;
+        }
+
+        public bool fn_EliminarSM(BE_SolicitudMantenimiento p_SMBE)
+        {
+            bool result = false;
+            result = DAO_SolicitudMantenimiento.Instancia.fn_EliminarSM(p_SMBE);
+
+            return result;
+        }
+
+        public BE_SolicitudMantenimiento fn_BuscaSM(BE_SolicitudMantenimiento p_SMBE)
+        {
+            BE_SolicitudMantenimiento result = null;
+            result = DAO_SolicitudMantenimiento.Instancia.fn_BuscaSM(p_SMBE);
+            return result;
+        }
+    }
+}
