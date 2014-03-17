3809d5c3565e43c8372d7f3cda7cfa8468e7d039
juan25vito <juan25vito@gmail.com>
2014-03-17 15:30:21 -0500

    Gestión de Mantenimiento 75%

---
+++ Fuentes/Net/ClinicaInternacional/CI.SIC.BL/BL_EvaluarSolicitudMantenimiento.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CI.SIC.BE;
+using CI.SIC.DA;
+
+namespace CI.SIC.BL
+{
+    public class BL_EvaluarSolicitudMantenimiento : BaseBL<BL_EvaluarSolicitudMantenimiento>
+    {
+        public List<BE_EvaluarSolicitudMantenimiento> fn_ListaESM(string fi, string ff)
+        {
+            List<BE_EvaluarSolicitudMantenimiento> result = null;
+            result = DAO_EvaluarSolicitudMantenimiento.Instancia.fn_ListaESM(fi,ff);
+            return result;
+        }
+
+        public List<BE_EvaluarSolicitudMantenimiento> fn_ConsultaESM_x_NroSolicitud(int nrosolicitud)
+        {
+            List<BE_EvaluarSolicitudMantenimiento> result = null;
+            result = DAO_EvaluarSolicitudMantenimiento.Instancia.fn_ConsultaESM_x_NroSolicitud(nrosolicitud);
+            return result;
+        }
+
+        public bool fn_ActualizarESM(BE_EvaluarSolicitudMantenimiento p_SMBE) 
+        {
+            bool result = false;
+            result = DAO_EvaluarSolicitudMantenimiento.Instancia.fn_ActualizarESM(p_SMBE);
+
+            return result;
+        }
+
+        public BE_EvaluarSolicitudMantenimiento fn_BuscaESM(BE_EvaluarSolicitudMantenimiento p_SMBE) 
+        {
+            BE_EvaluarSolicitudMantenimiento result = null;
+            result = DAO_EvaluarSolicitudMantenimiento.Instancia.fn_BuscaESM(p_SMBE);
+            return result;
+        }
+    }
+}
---
+++ Fuentes/Net/ClinicaInternacional/CI.SIC.BL/BL_OrdenTrabajo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CI.SIC.BE;
+using CI.SIC.DA;
+
+namespace CI.SIC.BL
+{
+    public class BL_OrdenTrabajo : BaseBL<BL_OrdenTrabajo>
+    {
+
+        public List<BE_OrdendeTrabajo> fn_MonitorearOT(int estado)
+        {
+            List<BE_OrdendeTrabajo> result = null;
+            result = DAO_OrdenTrabajo.Instancia.fn_MonitorearOT(estado);
+            return result;
+        }
+
+        public List<BE_OrdendeTrabajo> fn_ListaOT(string fi, string ff)
+        {
+            List<BE_OrdendeTrabajo> result = null;
+            result = DAO_OrdenTrabajo.Instancia.fn_ListaOT(fi, ff);
+            return result;
+        }
+
+        public bool fn_InsertarOT(BE_OrdendeTrabajo p_OTBE)
+        {
+            bool result = false;
+            result = DAO_OrdenTrabajo.Instancia.fn_InsertarOT(p_OTBE);
+
+            return result;
+        }
+
+        public bool fn_ActualizarOT(BE_OrdendeTrabajo p_OTBE)
+        {
+            bool result = false;
+            result = DAO_OrdenTrabajo.Instancia.fn_ActualizarOT(p_OTBE);
+
+            return result;
+        }
+
+        public bool fn_cerrarOT(BE_OrdendeTrabajo p_OTBE)
+        {
+            bool result = false;
+            result = DAO_OrdenTrabajo.Instancia.fn_CerrarOT(p_OTBE);
+
+            return result;
+        }
+
+        public bool fn_EliminarOT(BE_OrdendeTrabajo p_OTBE)
+        {
+            bool result = false;
+            result = DAO_OrdenTrabajo.Instancia.fn_EliminarOT(p_OTBE);
+
+            return result;
+        }
+
+
+
+        public BE_OrdendeTrabajo fn_BuscaOT(BE_OrdendeTrabajo p_OTBE)
+        {
+            BE_OrdendeTrabajo result = null;
+            result = DAO_OrdenTrabajo.Instancia.fn_BuscaOT(p_OTBE);
+            return result;
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
